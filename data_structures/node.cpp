9dad5b0b551290fdae7354e9a6ab2d9a67c64b67
Giacomo Balloccu <gballoccu@MacBook-Pro-di-Giacomo.local>
2020-03-15 21:23:16 +0100

    Node specialized in subclasses (Source)

--- data_structures/node.cpp
+++ data_structures/node.cpp
@@ -1,41 +1,86 @@
 #include "node.h"
 
+/*Node Constructors*/
 Node::Node(){
     this->right = nullptr;
     this->left = nullptr;
 };
-
-Node::Node(Node *left, Node *right, node_type type){
+Node::Node(Node *left, Node *right){
     this->right = right;
     this->left = left;
-    this->type = type;
 }
 
+/*Node Methods*/
 Node Node::getLeft() const{
     return *left;
 };
 Node Node::getRight() const{
     return *right;
 };
-node_type Node::getType() const{
-    return type;
-};
-
 void Node::setRight(Node *right){
     this->right = right;
 };
-
 void Node::setLeft(Node *left){
     this->left = left;
 };
-
-void Node::setType(node_type type){
-    this->type = type;
+NodeType Node::getType() const{
+    return None;
+};
+void Node::clear() {
+    left = nullptr;
+    right = nullptr;
 };
 
-void Node::clear(){
-    delete right;
-    delete left;
-    // To check
-    delete this;
-}
+/*XNode Constructors*/
+XNode::XNode(){
+    Node();
+};
+XNode::XNode(Node *left, Node *right){
+    Node(left, right);
+};
+
+/*XNode Methods*/
+NodeType XNode::getType() const{
+    return xNode;
+};
+void XNode::clear(){
+    point.~Point2();
+    left = nullptr;
+    right = nullptr;
+};
+
+/*YNode Constructors*/
+YNode::YNode(){
+    Node();
+};
+YNode::YNode(Node *left, Node *right){
+    Node(left, right);
+};
+
+/*YNode Methods*/
+NodeType YNode::getType() const{
+    return yNode;
+};
+void YNode::clear(){
+    segment.~Segment();
+    right = nullptr;
+    left = nullptr;
+};
+
+/*LeafNode Constructors*/
+LeafNode::LeafNode(){
+    Node();
+};
+LeafNode::LeafNode(Node *left, Node *right){
+    Node(left, right);
+};
+
+/*LeafNode Methods*/
+NodeType LeafNode::getType() const{
+    return leafNode;
+};
+void LeafNode::clear(){
+    trapezoid.clear();
+    right = nullptr;
+    left = nullptr;
+};
