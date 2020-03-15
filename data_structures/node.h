add3c57c8e5cb13aab368083bac19f3983f02d6c
Giacomo Balloccu <gballoccu@MacBook-Pro-di-Giacomo.local>
2020-03-15 21:22:50 +0100

    Node specialized in subclasses (Header)

--- data_structures/node.h
+++ data_structures/node.h
@@ -1,33 +1,101 @@
 #ifndef NODE_H
 #define NODE_H
-#include <cg3/geometry/segment2.h>
 
-enum node_type{p, s, t};
+#include <cg3/geometry/segment2.h>
+#include "trapezoid.h"
+
+/*Reference: Pag 27
+ * DAG:
+    type x nodes linked to points
+    type y nodes linked to segments
+    trapezoids stored in the leaves
+*/
+enum NodeType{
+ None,
+ xNode,
+ yNode,
+ leafNode,
+};
+
+/*
+
+General Definition for the Node class used in the DAG, used as main class for the type of nodes
+that inherit from this node class.
+
+*/
 
 class Node{
-private:
-    node_type type;
+protected:
     Node *left, *right;
-    /*To check: List of nodes for fathers?*/
 
-    /*Need refactor after the Trapezoid class creation*/
-    int data;
-    /*Value can be a Trapezoid a point or a segment*/
 public:
 
     Node();
-    Node(Node *right, Node *left, node_type type);
+    Node(Node *right, Node *left);
 
     void setRight(Node *right);
     void setLeft(Node *left);
-    void setType(node_type type);
 
+    virtual NodeType getType() const;
     Node getLeft() const;
     Node getRight() const;
-    node_type getType() const;
 
-    void clear();
+    virtual void clear();
 };
+
+
+/*Inheritance with type of Node*/
+
+/*
+
+This XNode class rappresent endpoints in the DAG
+
+*/
+class XNode : Node{
+private:
+    cg3::Point2d point;
+public:
+    XNode();
+    XNode(Node *left, Node *right);
+
+    NodeType getType() const override;
+    void clear() override;
+
+};
+
+/*
+
+This YNode class rappresents segments in the DAG
+
+*/
+class YNode : Node{
+private:
+    cg3::Segment2d segment;
+public:
+    YNode();
+    YNode(Node *left, Node *right);
+
+    NodeType getType() const override;
+    void clear() override;
+};
+
+
+/*
+
+This LeafNode class rappresents Trapezoids in the DAG
+
+*/
+class LeafNode : Node{
+private:
+    Trapezoid trapezoid;
+public:
+    LeafNode();
+    LeafNode(Node *left, Node *right);
+
+    NodeType getType() const override;
+    void clear() override;
+};
+
 
 #endif // NODE_H
 
