8d5a879e1e38fb38159284ba13a04ee8530865d5
Giacomo Balloccu <gballoccu@MacBook-Pro-di-Giacomo.local>
2020-03-17 23:09:04 +0100

    Added neighbor trapezoids and setters/getters + computeBoundingBoxMethod (need fixes)

--- data_structures/trapezoid.cpp
+++ data_structures/trapezoid.cpp
@@ -1,8 +1,21 @@
 #include "trapezoid.h"
 
-Trapezoid::Trapezoid()
-{
+Trapezoid::Trapezoid(){
+    topNeighbor = nullptr;
+    botNeighbor = nullptr;
+    rightNeighbor = nullptr;
+    leftNeighbor = nullptr;
+}
 
+Trapezoid::Trapezoid(cg3::Segment2d top, cg3::Segment2d bottom){
+    this->top = top;
+    this->bottom = bottom;
+    this->leftp = top.p1();
+    this->rightp = bottom.p2();
+    topNeighbor = nullptr;
+    botNeighbor = nullptr;
+    rightNeighbor = nullptr;
+    leftNeighbor = nullptr;
 }
 
 
@@ -11,7 +24,24 @@ void Trapezoid::clear(){
     bottom.~Segment();
     rightp.~Point2();
     leftp.~Point2();
+    topNeighbor = nullptr;
+    botNeighbor = nullptr;
+    leftNeighbor = nullptr;
+    rightNeighbor = nullptr;
     delete this;
+};
+
+cg3::Polygon2 Trapezoid::computeTrapezoid() const{
+  cg3::Polygon2 p = cg3::Polygon2();
+
+  /*Metto i punti clockwise(To check) in un polygono per calcolare il bounding box*/
+  /*TO CHECK: SBAGLIATO serve un albero per vedere sia x che y*/
+  p.pushBack(std::max(top.p1().y(), leftp.y()));
+  p.pushBack(std::max(top.p2().x(), rightp.x()));
+  p.pushBack(std::min(top.p1().x(), leftp.x()));
+  p.pushBack(std::max(top.p2().x(), rightp.x()));
+  p.updateBoundingBox();
+  return p;
 };
 /*
 
@@ -30,22 +60,46 @@ cg3::Point2d Trapezoid::getLeftp() const{
 cg3::Point2d Trapezoid::getRightp() const{
     return rightp;
 }
-
+Trapezoid* Trapezoid::getTopNeighbor() const{
+    return topNeighbor;
+};
+Trapezoid* Trapezoid::getBotNeighbor() const{
+    return botNeighbor;
+};
+Trapezoid* Trapezoid::getLeftNeighbor() const{
+    return leftNeighbor;
+};
+Trapezoid* Trapezoid::getRightNeighbor() const{
+    return rightNeighbor;
+};
 /*
 
 Setter
 
 */
-void Trapezoid::setTop(cg3::Segment2d top){
-    this->top = top;
-}
-void Trapezoid::setBottom(cg3::Segment2d bottom){
-    this->bottom = bottom;
-}
-void Trapezoid::setLeftp(cg3::Point2d leftp){
-    this->leftp = leftp;
-}
-void Trapezoid::setRightp(cg3::Point2d rightp){
-    this->rightp = rightp;
-}
+void Trapezoid::setTop(cg3::Segment2d s){
+    top = s;
+};
+void Trapezoid::setBottom(cg3::Segment2d s){
+    bottom = s;
+};
+void Trapezoid::setLeftp(cg3::Point2d p){
+    leftp = p;
+};
+void Trapezoid::setRightp(cg3::Point2d p){
+    rightp = p;
+};
+void Trapezoid::setTopNeighbor(Trapezoid *t){
+    topNeighbor = t;
+};
+void Trapezoid::setBotNeighbor(Trapezoid *t){
+     botNeighbor = t;
+};
+void Trapezoid::setLeftNeighbor(Trapezoid *t){
+     leftNeighbor = t;
+};
+void Trapezoid::setRightNeighbor(Trapezoid *t){
+     rightNeighbor = t;
+};
+
 
--- data_structures/trapezoid.h
+++ data_structures/trapezoid.h
@@ -2,6 +2,8 @@
 #define TRAPEZOID_H
 
 #include <cg3/geometry/segment2.h>
+#include <cg3/geometry/bounding_box2.h>
+#include <cg3/geometry/polygon2.h>>
 
 class Trapezoid
 {
@@ -13,22 +15,38 @@ private:
     cg3::Point2d leftp;
     cg3::Point2d rightp;
 
+    Trapezoid *topNeighbor;
+    Trapezoid *botNeighbor;
+    Trapezoid *leftNeighbor;
+    Trapezoid *rightNeighbor;
    /*To check: Neighborhoods?*/
 public:
     Trapezoid();
+    Trapezoid(cg3::Segment2d top, cg3::Segment2d bottom);
 
     cg3::Segment2d getTop() const;
     cg3::Segment2d getBottom() const;
     cg3::Point2d getLeftp() const;
     cg3::Point2d getRightp() const;
 
-    void setTop(cg3::Segment2d);
-    void setBottom(cg3::Segment2d);
-    void setLeftp(cg3::Point2d);
-    void setRightp(cg3::Point2d);
+    Trapezoid* getTopNeighbor() const;
+    Trapezoid* getBotNeighbor() const;
+    Trapezoid* getLeftNeighbor() const;
+    Trapezoid* getRightNeighbor() const;
+
+    void setTop(cg3::Segment2d s);
+    void setBottom(cg3::Segment2d s);
+    void setLeftp(cg3::Point2d p);
+    void setRightp(cg3::Point2d p);
+
+    void setTopNeighbor(Trapezoid *t);
+    void setBotNeighbor(Trapezoid *t);
+    void setLeftNeighbor(Trapezoid *t);
+    void setRightNeighbor(Trapezoid *t);
+
+    cg3::Polygon2 computeTrapezoid() const;
 
     void clear();
-    //getArea(top, bottom, leftp, rightp)
 };
 
 #endif // TRAPEZOID_H
