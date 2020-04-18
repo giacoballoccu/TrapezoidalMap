77dccf70583f2cf3bb77be34c37cf1a0521fcd2c
giacoballoccu <giacoballoccu@live.it>
2020-04-18 12:11:34 +0200

    Node class inheritance completly removed In favour of a unique class Node. The node is specialized by the pre- existent enum (xNode, yNode, leafNode) and it store the id for the query in the Tmap vectors istead of containg the object.

--- data_structures/node.cpp
+++ data_structures/node.cpp
@@ -1,91 +1,50 @@
 #include "node.h"
 
 /*Node Constructors*/
-Node::Node(){
-    this->right = nullptr;
+Node::Node(NodeType type, const size_t& id){
+    this->type = type;
+    this->id = id;
     this->left = nullptr;
+    this->right = nullptr;
 };
 
+
 /*Node Methods*/
+Node*& Node::getLeftRfr(){
+    return left;
+};
+Node*& Node::getRightRfr(){
+    return right;
+};
 Node* Node::getLeft() const{
     return left;
 };
 Node* Node::getRight() const{
     return right;
 };
-void Node::setRight(Node *right){
-    this->right = right;
-};
+
 void Node::setLeft(Node *left){
     this->left = left;
 };
-NodeType Node::getType() const{
-    return undefinedNode;
+void Node::setRight(Node *right){
+    this->right = right;
 };
+
+void Node::setLeftRfr(Node *&left){
+    this->left = left;
+};
+void Node::setRightRfr(Node *&right){
+    this->right = right;
+};
+
+//virtual void setTrapezoidId(const size_t& id);
+NodeType Node::getType() const{
+    return type;
+};
+
 void Node::clear() {
     left = nullptr;
     right = nullptr;
+    delete this;
 };
 
-/*XNode Constructors*/
-
-XNode::XNode(cg3::Point2d point){
-    Node();
-    this->point = point;
-};
-
-
-/*XNode Methods*/
-cg3::Point2d XNode::getPoint() const{
-    return point;
-};
-NodeType XNode::getType() const{
-    return xNode;
-};
-void XNode::clear(){
-    point.~Point2();
-    this->Node::clear();
-};
-
-/*YNode Constructors*/
-cg3::Segment2d YNode::getSegment() const{
-    return segment;
-};
-
-YNode::YNode(cg3::Segment2d segment){
-    Node();
-    this->segment = segment;
-};
-
-/*YNode Methods*/
-NodeType YNode::getType() const{
-    return yNode;
-};
-void YNode::clear(){
-    segment.~Segment();
-    this->Node::clear();
-};
-
-/*LeafNode Constructors*/
-
-LeafNode::LeafNode(Trapezoid *trapezoid){
-    Node();
-    this->trapezoid = trapezoid;
-    this->trapezoid->node = this;
-};
-
-
-/*LeafNode Methods*/
-Trapezoid* LeafNode::getTrapezoid() const{
-    return trapezoid;
-};
-
-
-
-NodeType LeafNode::getType() const{
-    return leafNode;
-};
-void LeafNode::clear(){
-    trapezoid = nullptr;
-    this->Node::clear();
-};
--- data_structures/node.h
+++ data_structures/node.h
@@ -25,76 +25,36 @@ that inherit from this node class.
 
 class Node{
 private:
-// std::list<Node*> parents;
-public:
-    Node *left, *right; //In xNode geometrical left and right, in yNode geometrical above and below, in leafNode nullptr
+    Node *left, *right;
+    size_t id;
+    NodeType type;
 
-    Node();
+public:
+
+    Node(NodeType type, const size_t& id);
+    Node*& getLeftRfr();
+    Node*& getRightRfr();
     Node* getLeft() const;
     Node* getRight() const;
 
+    size_t& getId(){
+        return id;
+    };
+
+    void reduceId(){
+        id--;
+    }
+
+    void setLeftRfr(Node *&left);
+    void setRightRfr(Node *&right);
     void setLeft(Node *left);
     void setRight(Node *right);
-
-    virtual NodeType getType() const;
-
-    virtual void clear();
+    //virtual void setTrapezoidId(const size_t& id);
+    NodeType getType() const;
+    void clear();
 };
 
 
-/*Inheritance with type of Node*/
-
-/*
-
-This XNode class rappresent endpoints in the DAG
-
-*/
-class XNode : public Node{
-private:
-    cg3::Point2d point;
-public:
-    XNode(cg3::Point2d point);
-
-    cg3::Point2d getPoint() const;
-    NodeType getType() const override;
-
-    void clear() override;
-
-};
-
-/*
-
-This YNode class rappresents segments in the DAG
-
-*/
-class YNode : public Node{
-private:
-    cg3::Segment2d segment;
-public:
-    YNode(cg3::Segment2d segment);
-
-    cg3::Segment2d getSegment() const;
-    NodeType getType() const override;
-
-    void clear() override;
-};
-
-
-/*
-
-This LeafNode class rappresents Trapezoids in the DAG
-
-*/
-class LeafNode : public Node{
-private:
-    Trapezoid *trapezoid;
-public:
-    LeafNode(Trapezoid* trapezoid);
-
-    Trapezoid* getTrapezoid() const;
-    NodeType getType() const override;
-    void clear() override;
-};
 
 
 #endif // NODE_H
